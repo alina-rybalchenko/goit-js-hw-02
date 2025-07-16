a4443aa14d570e2a9a219fc138880b47f7f05edf
Alina <alinarybalchenko@hotmail.com>
2025-07-16 23:37:48 +0200

    Complete all tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,7 @@
-function checkForSpam(message) {}
+function checkForSpam(message) {
+  const lowerCaseMessage = message.toLowerCase();
+  return lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
+}
 
 console.log(checkForSpam('Latest technology news')); // false
 console.log(checkForSpam('JavaScript weekly newsletter')); // false
